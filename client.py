4069c9d59e4b83b967498dfae85f4c14ecb4fa2f
CSA-G <gaminginworld19@gmail.com>
2025-04-07 22:15:06 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from google import genai
-gapi="AIzaSyCrYWQPt7KfC-kuBiGev_4LXcdmHJN9698"
-client = genai.Client(api_key="AIzaSyCrYWQPt7KfC-kuBiGev_4LXcdmHJN9698")
+gapi= # "USE YOUR GOOGLE GEMINI API 
+client = genai.Client(api_key= # "USE YOUR GOOGLE GEMINI API )
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="You are a Virtual assistant name Jarvis and you will perform all the general tasks which I will command just like Alexa and Google "
